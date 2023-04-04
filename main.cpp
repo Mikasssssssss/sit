04669b0135685ae362a204a6ced1846a946c5c3f
David <justmikasss@gmail.com>
2023-04-04 23:25:55 +0300

    Обновил программу в main.cpp добавил в программу функцию `fib(n)`, которая будет рекурсивно вычислять n-ое число Фибоначчи при условии, что последовательность начинается с (1, 1, ...). Например, `fib(10) = 55`. Программа должна вывести вычисленное значение в стандартный вывод

--- main.cpp
+++ main.cpp
@@ -2,11 +2,13 @@
 using namespace std;
 
 int fib(int n) {
-	if (n == 0) return 0;
+	if (n == 0) return 1;
 	if (n == 1) return 1;
 	return f(n - 1) + f(n - 2);
 }
 
 int main() {
-	cout << "Hello World";
+	int n;
+	ci>>n;
+	cout<<fib(n);
 }
