0d22f770bd1703a0aa17c0f49ac7dcb23a0b2045
David <justmikasss@gmail.com>
2023-04-04 23:30:38 +0300

    Исправил программу программу так, чтобы последовательность чисел начиналась со значений (0, 1, ...). Таким образом, `fib(10) = 34`;

--- main.cpp
+++ main.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 
 int fib(int n) {
-	if (n == 0) return 1;
+	if (n == 0) return 0;
 	if (n == 1) return 1;
 	return f(n - 1) + f(n - 2);
 }
 
 int main() {
 	int n;
-	ci>>n;
-	cout<<fib(n);
+	cin»n;
+	cout«fib(n);
 }
